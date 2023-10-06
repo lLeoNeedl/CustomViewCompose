284bbb014bf282b50730a3ce8e9250efbfa16c4a
Leonid Lavrov <274852@gmail.com>
2023-10-07 00:01:17 +0300

    Canvas third lesson

--- app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
@@ -2,92 +2,82 @@ package com.example.customviewcompose.samples
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun CanvasTest() {
+    var points by remember {
+        mutableStateOf(listOf<Point>())
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(
-                        Color.Cyan,
-                        Color.Magenta
-                    ),
-                    start = Offset(100.dp.toPx(), 0F),
-                    end = Offset(200.dp.toPx(), 200.dp.toPx()),
-                    tileMode = TileMode.Mirror
-                )
+                color = Color.White
             )
+            .pointerInput(key1 = Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        points = points + Point(it, true)
+                    },
+                    onDrag = { change, _ ->
+                        points = points + change.historical.map {
+                            Point(
+                                offset = it.position,
+                                false
+                            )
+                        }
+                    }
+                )
+            }
     ) {
+        val path = Path()
+        points.forEach { point ->
+            if (point.isStartPosition) {
+                path.moveTo(point.offset.x, point.offset.y)
+            } else {
+                path.lineTo(point.offset.x, point.offset.y)
+            }
+        }
         drawPath(
-            path = Path().apply {
-                moveTo(
-                    x = center.x,
-                    y = 100.dp.toPx()
+            path = path,
+            brush = Brush.linearGradient(
+                listOf(
+                    Color.Cyan,
+                    Color.Magenta,
+                    Color.Red
                 )
-                lineTo(
-                    x = center.x + 25.dp.toPx(),
-                    y = 150.dp.toPx()
-                )
-                lineTo(
-                    x = center.x + 75.dp.toPx(),
-                    y = 150.dp.toPx()
-                )
-                lineTo(
-                    x = center.x + 45.dp.toPx(),
-                    y = 195.dp.toPx()
-                )
-                lineTo(
-                    x = center.x + 60.dp.toPx(),
-                    y = 250.dp.toPx()
-                )
-                lineTo(
-                    x = center.x,
-                    y = 220.dp.toPx()
-                )
-                lineTo(
-                    x = center.x - 60.dp.toPx(),
-                    y = 250.dp.toPx()
-                )
-                lineTo(
-                    x = center.x - 45.dp.toPx(),
-                    y = 195.dp.toPx()
-                )
-                lineTo(
-                    x = center.x - 75.dp.toPx(),
-                    y = 150.dp.toPx()
-                )
-                lineTo(
-                    x = center.x - 25.dp.toPx(),
-                    y = 150.dp.toPx()
-                )
-                lineTo(
-                    x = center.x,
-                    y = 100.dp.toPx()
-                )
-            },
-            color = Color.White,
-            style = Fill
+            ),
+            style = Stroke(width = 10.dp.toPx())
         )
     }
 }
+
+data class Point(
+    val offset: Offset,
+    val isStartPosition: Boolean
+)
 
 @Composable
 fun Oleg() {
